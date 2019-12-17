2e29df62c773e00d67fb5a75041525e7d0142759
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-12-17 14:58:01 +0100

    fixed mysql full_group_by issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## [1.0.5] - 2019-12-17
+- updated license
+- fixed only_full_group_by issue with MySQL 5.7
+
 ## [1.0.4] - 2018-04-04
 
 ### Added
--- models/GeonamesPostalModel.php
+++ models/GeonamesPostalModel.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Contao Open Source CMS
- * 
+ *
  * Copyright (c) 2015 Heimrich & Hannot GmbH
  * @package geonames
  * @author Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
@@ -11,7 +11,11 @@
 namespace HeimrichHannot\Geonames;
 
 
-class GeonamesPostalModel extends \Model
+use Contao\Database;
+use Contao\Model;
+use Contao\Model\Collection;
+
+class GeonamesPostalModel extends Model
 {
 	protected static $strTable = 'tl_geonames_postal';
 
@@ -21,20 +25,22 @@ class GeonamesPostalModel extends \Model
 	 * @param string  $varValue   The city value
 	 * @param array   $arrOptions An optional options array
 	 *
-	 * @return \Model\Collection|null A collection of models or null if the title was not found
+	 * @return Collection|null A collection of models or null if the title was not found
 	 */
-	public static function findByCity($varValue, array $arrOptions=array())
+	public static function findByCity($varValue, array $arrOptions= [])
 	{
 		$t = static::$strTable;
 
 		return static::findBy(array("LOWER($t.city) LIKE '" . strval(strtolower($varValue)) . "'"), $arrOptions);
 	}
 
-	public static function findDistinctCitiesByCountries(array $arrCountries = array('DE'), array $arrOptions = array())
+	public static function findDistinctCitiesByCountries(array $arrCountries = ['DE'], array $arrOptions = [])
 	{
 		$t = static::$strTable;
 
-                $objResult = \Database::getInstance()->prepare("SELECT city FROM $t WHERE country IN('" . implode("','", $arrCountries) . "') GROUP BY BINARY city ORDER BY city")->execute();
+                $objResult = Database::getInstance()
+                    ->prepare("SELECT CAST(city as BINARY) AS city FROM $t WHERE country IN('" . implode("','", $arrCountries) . "') GROUP BY city ORDER BY city")
+                    ->execute();
 
 		$arrResult = array();
 
